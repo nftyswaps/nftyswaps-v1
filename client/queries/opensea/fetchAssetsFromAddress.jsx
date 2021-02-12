e131b4b43c99d98ab753c845cf9b263891a98d48
mauricechristopher <mchristo@student.42.us.org>
2021-02-12 06:22:08 -0500

    queries now modular

--- client/queries/opensea/fetchAssetsFromAddress.jsx
+++ client/queries/opensea/fetchAssetsFromAddress.jsx
@@ -1,7 +1,5 @@
 const fetchCurrentUserAssets = (setData, addressToQuery) => {
 	fetch(
-		// `https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20&owner=${userAccount}`,
-
 		`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20&owner=${addressToQuery}`,
 		{
 			method: 'GET',
--- client/queries/opensea/fetchCurrentUserAssets.jsx
+++ client/queries/opensea/fetchCurrentUserAssets.jsx
@@ -2,9 +2,7 @@ import { useWallet } from 'use-wallet'
 
 const fetchCurrentUserAssets = (setData, userAccount) => {
 	fetch(
-		// `https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20&owner=${userAccount}`,
-
-		`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20&owner=0xEE5A82516DC71BA3Cb0Cc2056095E41FB894ae2D`,
+		`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20&owner=${userAccount}`,
 		{
 			method: 'GET',
 		}
