78b84ad77f26a992245446da5fcf2b554195afdc
mauricechristopher <36680664+mauricechristopher@users.noreply.github.com>
2021-02-17 22:57:38 -0500

    added close button to banner

--- client/components/Banner/BannerStyles.jsx
+++ client/components/Banner/BannerStyles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Close } from '@styled-icons/evil/Close'
 
 export const BannerWrapper = styled.div`
 	width: 100%;
@@ -7,10 +8,22 @@ export const BannerWrapper = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
+	padding: 0 1em;
+	transition: all 0.42s ease-in;
+	transform: ${({ isHidden }) =>
+		isHidden ? 'translateY(-5vh); height: 0;' : 'translateY(0)'};
 `
 
-export const BannerWords = styled.h1`
+export const BannerWords = styled.h2`
 	font-family: ${({ theme }) => theme.fonts.montserrat};
 	color: ${({ theme }) => theme.colors.primaryGray};
 	font-size: 16px;
+	position: fixed;
+`
+
+export const BannerClose = styled(Close)`
+	height: 24px;
+	width: 24px;
+	margin-left: auto;
+	cursor: pointer;
 `
--- client/components/Banner/index.jsx
+++ client/components/Banner/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { BannerWrapper, BannerWords } from './BannerStyles'
+import React, { useState } from 'react'
+import { BannerWrapper, BannerWords, BannerClose } from './BannerStyles'
 
-const Banner = () => (
-	<BannerWrapper>
-		<BannerWords>
-			Project under construction, please use with caution. Connect with
-			Metamask via the Rinkeby testnet
-		</BannerWords>
-	</BannerWrapper>
-)
+const Banner = () => {
+	const [isHidden, setIsHidden] = useState(false)
+	console.log(isHidden)
+	const handleClick = () => {
+		setIsHidden(true)
+	}
+	return (
+		<BannerWrapper isHidden={isHidden}>
+			<BannerWords>
+				Project under construction, please use with caution. Connect
+				with Metamask via the Rinkeby testnet
+			</BannerWords>
+			<BannerClose onClick={() => handleClick()} />
+		</BannerWrapper>
+	)
+}
 
 export default Banner
