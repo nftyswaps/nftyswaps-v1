49016c87bcc7e8702b4babfd1bf9a966e9002fd3
mauricechristopher <36680664+mauricechristopher@users.noreply.github.com>
2021-03-03 13:29:51 -0500

    upgrades to faucet window, can now input a token id on frontend, slight style update as well

--- client/components/FaucetWindow/Styles/FaucetWindowStyles.jsx
+++ client/components/FaucetWindow/Styles/FaucetWindowStyles.jsx
@@ -5,7 +5,7 @@ export const FaucetWrapper = styled.div`
 
 	display: flex;
 	flex-direction: column;
-	justify-content: flex-start;
+	justify-content: space-between;
 	align-items: center;
 	overflow: hidden;
 
@@ -17,7 +17,7 @@ export const FaucetWrapper = styled.div`
 
 	@media (min-width: ${({ theme }) => theme.device.tablet}) {
 		padding: 2.5vh 2.5vw;
-		width: 50vw;
+		width: 70vw;
 	}
 
 	@media (min-width: ${({ theme }) => theme.device.laptop}) {
@@ -26,8 +26,76 @@ export const FaucetWrapper = styled.div`
 	}
 `
 
+export const InnerFaucetWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	height: 100%;
+	width: 100%;
+`
+
+export const FaucetTitle = styled.h1`
+	margin: 0;
+	font-family: ${({ theme }) => theme.fonts.raleway};
+	color: ${({ theme }) => theme.colors.white};
+	font-variant-caps: all-small-caps;
+	text-align: center;
+	text-shadow: 5px 5px 0.5rem #000;
+	letter-spacing: 7px;
+
+	width: 100%;
+
+	font-size: 24px;
+
+	@media (min-width: ${({ theme }) => theme.device.tablet}) {
+		font-size: 36px;
+	}
+	@media (min-width: ${({ theme }) => theme.device.laptopL}) {
+		font-size: 48px;
+	}
+`
+
 export const MintButton = styled.button`
+	cursor: pointer;
 	height: 5vh;
-	width: 10vw;
 	font-size: 16px;
+	border: none;
+	border-radius: 7px;
+	box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+
+	background: ${({ theme }) => theme.colors.orange};
+	color: ${({ theme }) => theme.colors.white};
+
+	width: 35vw;
+	@media (min-width: ${({ theme }) => theme.device.tablet}) {
+		width: 20vw;
+	}
+`
+
+export const FaucetInput = styled.input.attrs({
+	type: 'number',
+	placeholder: 'Token ID i.e. <42>',
+})`
+	font-family: ${({ theme }) => theme.fonts.montserrat};
+	background-color: ${({ theme }) => theme.colors.tertiaryGray};
+	color: ${({ theme }) => theme.colors.white};
+	text-overflow: fade();
+	/* padding-right: 70px; */
+	padding-left: 20px;
+	border-radius: 7px;
+	height: 5vh;
+	width: 45vw;
+
+	@media (min-width: ${({ theme }) => theme.device.tablet}) {
+		width: 20vw;
+	}
+`
+
+export const InputWithButtonWrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	height: auto;
+	width: 100%;
 `
--- client/components/FaucetWindow/index.jsx
+++ client/components/FaucetWindow/index.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 
 import useIsLoggedIn from '../../hooks/useIsLoggedIn'
 import { SingleWindowWrapper } from '../../global/globalStyles/SingleWindowStyles'
-import { FaucetWrapper, MintButton } from './Styles/FaucetWindowStyles'
+import {
+	FaucetWrapper,
+	InnerFaucetWrapper,
+	MintButton,
+	FaucetTitle,
+	InputWithButtonWrapper,
+	FaucetInput,
+} from './Styles/FaucetWindowStyles'
 import WelcomeBox from '../SearchWindow/WelcomeBox'
 import { useWallet } from 'use-wallet'
 import getContracts from '../../hooks/getContracts'
+import useInputChange from '../../hooks/useInputChange'
 
 const FaucetWindow = () => {
 	const wallet = useWallet()
 	const isLoggedIn = useIsLoggedIn()
 	const contracts = getContracts()
+	const [input, handleInputChange] = useInputChange()
 
 	console.log(contracts)
-
 	const mintTokenA = async () => {
 		const aResult = await contracts.TokenOneMinter.methods
-			.mint(wallet.account, 5)
+			.mint(wallet.account, input.TokenOneMinter)
 			.send({ from: wallet.account })
 
 		if (aResult.transactionHash)
@@ -26,44 +34,38 @@ const FaucetWindow = () => {
 	const mintTokenB = async () => {
 		console.log(
 			await contracts.TokenTwoMinter.methods
-				.mint(wallet.account, 4)
+				.mint(wallet.account, input.TokenTwoMinter)
 				.send({ from: wallet.account })
 		)
 	}
-
-	// const Checks = async () => {
-	// 	const numberOfTokens = await contracts.TokenTwoMinter.methods.balanceOf(
-	// 		wallet.account
-	// 	)
-	// 	console.log(numberOfTokens)
-	// 	return (
-	// 		<div>
-	// 			{numberOfTokens.map(async (token) => (
-	// 				<h1>
-	// 					{
-	// 						await contracts.TokenTwoMinter.methods.tokenOfOwnerByIndex(
-	// 							wallet.account,
-	// 							i
-	// 						)
-	// 					}
-	// 				</h1>
-	// 			))}
-	// 		</div>
-	// 	)
-	// console.log(tokens)
-	// console.log(
-	// 	await contracts.TokenTwoMinter.methods
-	// 		.tokenOfOwnerByIndex(wallet.account, 0)
-	// 		.call()
-	// )
-	// }
 
 	return (
 		<SingleWindowWrapper>
 			{isLoggedIn === 'connected' ? (
 				<FaucetWrapper>
-					<MintButton onClick={mintTokenA}>Mint Token A</MintButton>
-					<MintButton onClick={mintTokenB}>Mint Token B</MintButton>
+					<FaucetTitle>Faucet</FaucetTitle>
+					<InnerFaucetWrapper>
+						<InputWithButtonWrapper>
+							<FaucetInput
+								onChange={handleInputChange}
+								name='TokenOneMinter'
+							/>
+							<MintButton onClick={mintTokenA}>
+								Mint Token A
+							</MintButton>
+						</InputWithButtonWrapper>
+
+						<InputWithButtonWrapper>
+							<FaucetInput
+								onChange={handleInputChange}
+								name='TokenTwoMinter'
+							/>
+							<MintButton onClick={mintTokenB}>
+								Mint Token B
+							</MintButton>
+						</InputWithButtonWrapper>
+					</InnerFaucetWrapper>
+
 					{/* <Checks /> */}
 				</FaucetWrapper>
 			) : (
@@ -74,3 +76,30 @@ const FaucetWindow = () => {
 }
 
 export default FaucetWindow
+
+// const Checks = async () => {
+// 	const numberOfTokens = await contracts.TokenTwoMinter.methods.balanceOf(
+// 		wallet.account
+// 	)
+// 	console.log(numberOfTokens)
+// 	return (
+// 		<div>
+// 			{numberOfTokens.map(async (token) => (
+// 				<h1>
+// 					{
+// 						await contracts.TokenTwoMinter.methods.tokenOfOwnerByIndex(
+// 							wallet.account,
+// 							i
+// 						)
+// 					}
+// 				</h1>
+// 			))}
+// 		</div>
+// 	)
+// console.log(tokens)
+// console.log(
+// 	await contracts.TokenTwoMinter.methods
+// 		.tokenOfOwnerByIndex(wallet.account, 0)
+// 		.call()
+// )
+// }
