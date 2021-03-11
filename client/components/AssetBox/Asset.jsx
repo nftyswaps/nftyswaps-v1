60e6f425141d4cdcdbd0bda32faf7b3d49f6c7a5
mauricechristopher <36680664+mauricechristopher@users.noreply.github.com>
2021-03-11 18:20:15 -0500

    added default image if token has no picture

--- client/components/AssetBox/Asset.jsx
+++ client/components/AssetBox/Asset.jsx
@@ -7,12 +7,16 @@ import {
 } from './Styles/AssetBoxStyles'
 
 const Asset = ({ asset, handleClick }) => {
-	return (
+	return !asset.detail ? (
 		<AssetWrapper
 			key={asset.id}
 			onClick={handleClick ? () => handleClick(asset) : null}
 		>
-			<AssetThumb src={asset.image_url} />
+			<AssetThumb
+				src={
+					asset.image_url || 'https://i.ibb.co/kM4htMP/not-found.png'
+				}
+			/>
 			<AssetTitle>{asset.name || asset.token_id}</AssetTitle>
 			<AssetArtist>
 				{asset.collection.name ||
@@ -20,6 +24,8 @@ const Asset = ({ asset, handleClick }) => {
 					'Unknown'}
 			</AssetArtist>
 		</AssetWrapper>
+	) : (
+		<AssetWrapper>{asset.detail}</AssetWrapper>
 	)
 }
 
