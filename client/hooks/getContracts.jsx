19486932d9a242db8b6100e41f2a1ddceea3b607
Kyle Longrich <longrichk@gmail.com>
2021-03-15 04:00:17 -0400

    updating ABI file path

--- client/hooks/getContracts.jsx
+++ client/hooks/getContracts.jsx
@@ -1,59 +1,59 @@
-import { useState } from 'react'
-import Web3 from 'web3'
-import { useWallet } from 'use-wallet'
+import { useState } from "react";
+import Web3 from "web3";
+import { useWallet } from "use-wallet";
 
-import TokenOneMinterDefinition from '../lib/contracts/TokenOneMinter.json'
-import TokenTwoMinterDefinition from '../lib/contracts/TokenTwoMinter.json'
-import NftSwapDefinition from '../lib/contracts/NftSwap.json'
+import TokenOneMinterDefinition from "../../build/contracts/TokenOneMinter.json";
+import TokenTwoMinterDefinition from "../../build/contracts/TokenTwoMinter.json";
+import NftSwapDefinition from "../../build/contracts/NftSwap.json";
 
 const getContracts = () => {
-	// get network ID and the deployed address
-	const wallet = useWallet()
-	const web3 = new Web3(Web3.givenProvider)
+  // get network ID and the deployed address
+  const wallet = useWallet();
+  const web3 = new Web3(Web3.givenProvider);
 
-	const [contracts, setContracts] = useState({
-		TokenOneMinter: null,
-		TokenTwoMinter: null,
-		NftSwap: null,
-	})
+  const [contracts, setContracts] = useState({
+    TokenOneMinter: null,
+    TokenTwoMinter: null,
+    NftSwap: null,
+  });
 
-	const networkId = wallet.chainId
+  const networkId = wallet.chainId;
 
-	const TokenOneMinterAddress =
-		TokenOneMinterDefinition.networks[networkId].address
+  const TokenOneMinterAddress =
+    TokenOneMinterDefinition.networks[networkId].address;
 
-	const TokenTwoMinterAddress =
-		TokenTwoMinterDefinition.networks[networkId].address
+  const TokenTwoMinterAddress =
+    TokenTwoMinterDefinition.networks[networkId].address;
 
-	const NftSwapAddress = NftSwapDefinition.networks[networkId].address
+  const NftSwapAddress = NftSwapDefinition.networks[networkId].address;
 
-	// create the instances
-	if (
-		contracts.NftSwap == null &&
-		contracts.TokenOneMinterAddress == null &&
-		contracts.TokenTwoMinterAddress == null
-	) {
-		const TokenOneMinterInstance = new web3.eth.Contract(
-			TokenOneMinterDefinition.abi,
-			TokenOneMinterAddress
-		)
+  // create the instances
+  if (
+    contracts.NftSwap == null &&
+    contracts.TokenOneMinterAddress == null &&
+    contracts.TokenTwoMinterAddress == null
+  ) {
+    const TokenOneMinterInstance = new web3.eth.Contract(
+      TokenOneMinterDefinition.abi,
+      TokenOneMinterAddress
+    );
 
-		const TokenTwoMinterInstance = new web3.eth.Contract(
-			TokenTwoMinterDefinition.abi,
-			TokenTwoMinterAddress
-		)
-		const NftSwapInstance = new web3.eth.Contract(
-			NftSwapDefinition.abi,
-			NftSwapAddress
-		)
+    const TokenTwoMinterInstance = new web3.eth.Contract(
+      TokenTwoMinterDefinition.abi,
+      TokenTwoMinterAddress
+    );
+    const NftSwapInstance = new web3.eth.Contract(
+      NftSwapDefinition.abi,
+      NftSwapAddress
+    );
 
-		setContracts({
-			TokenOneMinter: TokenOneMinterInstance,
-			TokenTwoMinter: TokenTwoMinterInstance,
-			NftSwap: NftSwapInstance,
-		})
-	}
-	return contracts
-}
+    setContracts({
+      TokenOneMinter: TokenOneMinterInstance,
+      TokenTwoMinter: TokenTwoMinterInstance,
+      NftSwap: NftSwapInstance,
+    });
+  }
+  return contracts;
+};
 
-export default getContracts
+export default getContracts;
