cb47911488e0d835029e2fd7a0dbe6770159c856
Kyle Longrich <longrichk@gmail.com>
2021-03-26 09:13:12 -0400

    adding erc20 to nft pairs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 const NftSwap = artifacts.require('./NftSwap.sol')
 const TokenOneMinter = artifacts.require('./Mocks/TokenOneMinter.sol')
 const TokenTwoMinter = artifacts.require('./Mocks/TokenTwoMinter.sol')
+const Erc20Minter = artifacts.require('./Erc20Minter.sol');
 
 module.exports = function (deployer) {
 	deployer.deploy(NftSwap)
 	deployer.deploy(TokenOneMinter)
 	deployer.deploy(TokenTwoMinter)
+	deployer.deploy(Erc20Minter)
 }
