1b9ac5eee2a8d0ad0414e4c1611753a7f1e761d3
Kyle Longrich <longrichk@gmail.com>
2021-03-31 09:37:18 -0400

    deploying new contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,12 @@ const NftSwap = artifacts.require('./NftSwap.sol')
 const TokenOneMinter = artifacts.require('./Mocks/TokenOneMinter.sol')
 const TokenTwoMinter = artifacts.require('./Mocks/TokenTwoMinter.sol')
 const Erc20Minter = artifacts.require('./Erc20Minter.sol');
+const NftToErcSwap = artifacts.require('./NftToErcSwap.sol')
 
 module.exports = function (deployer) {
 	deployer.deploy(NftSwap)
 	deployer.deploy(TokenOneMinter)
 	deployer.deploy(TokenTwoMinter)
 	deployer.deploy(Erc20Minter)
+	deployer.deploy(NftToErcSwap);
 }
