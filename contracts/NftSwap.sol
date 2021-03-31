b3e0b71c892a6dba96dbcd5c8b6ebfba3b37fd38
Kyle Longrich <longrichk@gmail.com>
2021-03-31 13:10:29 -0400

    allowing multi-order creation

--- contracts/NftSwap.sol
+++ contracts/NftSwap.sol
@@ -2,164 +2,79 @@ pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 contract NftSwap {
     event newOrder(
-        address makerAddress,
-        address takerAddress,
-        address makerContract,
-        uint256 makerID,
-        address takerContract,
-        uint256 takerID
-    );
-
-    event newErc20Order(
-        address ListerAddress,
-        address OfferAddress,
-        address NftTokenAddrss,
-        uint256 TokenId,
-        address Erc20Contract,
-        uint256 Amount
+        address ListserTokenAddress,
+        uint256 ListerTokenId,
+        address BuyerAddress,
+        address BuyerTokenAddress,
+        uint256 BuyerTokenId,
+        uint256 OrderID
     );
 
     struct OrderDetials {
-        address ListerAddress;
-        address NftTokenAddress;
-        uint256 TokenId;
-        uint256 Amount;
-        address Erc20Contract;
+        address ListerTokenAddress;
+        uint256 ListerTokenId;
+        address BuyerTokenAddress;
+        uint256 BuyerTokenId;
     }
 
-    mapping(address => OrderDetials) public OrderInfo;
+    mapping(address => mapping(uint256 => OrderDetials)) public OrderInfo;
 
-    //This makes my head hurt and seems like a usless "flex" that makes for unreadable code imo.
-    mapping(uint256 => mapping(address => mapping(uint256 => mapping(address => address))))
-        public orders;
-
-    function validRecipient(
-        address makerContract,
-        uint256 makerID,
-        address takerContract,
-        uint256 takerID
-    ) public view returns (bool) {
-
-            address recipient
-         = orders[makerID][makerContract][takerID][takerContract];
-        return recipient != address(0);
-    }
+    address[] public BuyerAddress;
 
     function makeOrder(
-        address makerContract,
-        uint256 makerID,
-        address takerContract,
-        uint256 takerID
+        address BuyerContract,
+        uint256 BuyerID,
+        address ListerContract,
+        uint256 ListerID,
+        uint256 OrderID
     ) public {
         require(
-            !validRecipient(makerContract, makerID, takerContract, takerID),
-            "invalid order"
-        );
-        require(
-            IERC721(makerContract).getApproved(makerID) == address(this),
+            IERC721(BuyerContract).getApproved(BuyerID) == address(this),
             "not approved, cannot create order"
         );
-
-        // only the token owner can create the order
         require(
-            IERC721(makerContract).ownerOf(makerID) == msg.sender,
+            IERC721(BuyerContract).ownerOf(BuyerID) == msg.sender,
             "not owner, cannot create order"
         );
-        // create the order
-        orders[makerID][makerContract][takerID][takerContract] = msg.sender;
 
-        // retrieve and emit the takerAddress
-        address takerAddress = IERC721(takerContract).ownerOf(takerID);
+        OrderInfo[msg.sender][OrderID].ListerTokenAddress = ListerContract;
+        OrderInfo[msg.sender][OrderID].ListerTokenId = ListerID;
+        OrderInfo[msg.sender][OrderID].BuyerTokenAddress = BuyerContract;
+        OrderInfo[msg.sender][OrderID].BuyerTokenId = BuyerID;
+
         emit newOrder(
+            ListerContract,
+            ListerID,
             msg.sender,
-            takerAddress,
-            makerContract,
-            makerID,
-            takerContract,
-            takerID
+            BuyerContract,
+            BuyerID,
+            OrderID
         );
     }
 
-    function takeOrder(
-        address makerContract,
-        uint256 makerID,
-        address takerContract,
-        uint256 takerID
-    ) public {
-        require(
-            validRecipient(makerContract, makerID, takerContract, takerID),
-            "invalid order"
-        );
-
+    function takeOrder(address Buyer, uint256 OrderID) public {
         // only the token owner can create the order
         require(
-            IERC721(takerContract).ownerOf(takerID) == msg.sender,
-            "not owner, cannot create order"
+            IERC721(OrderInfo[Buyer][OrderID].ListerTokenAddress).ownerOf(
+                OrderInfo[Buyer][OrderID].ListerTokenId
+            ) == msg.sender,
+            "not owner, cannot excute order"
         );
-
-
-            address recipient
-         = orders[makerID][makerContract][takerID][takerContract];
-
-        delete orders[makerID][makerContract][takerID][takerContract];
         // transfer the order maker token from maker to taker
-        IERC721(makerContract).transferFrom(recipient, msg.sender, makerID);
-        // transfer the order taker token from taker to maker
-        IERC721(takerContract).transferFrom(msg.sender, recipient, takerID);
-    }
-
-    function makeNftToERC20(
-        address ListerAddress,
-        address ListerNftContract,
-        uint256 TokenID,
-        address Erc20Contract,
-        uint256 Amount
-    ) public payable {
-        //Add Check to make sure Erc20Contract address passes is Valid
-        //Make sure sender has enough coins
-        require(
-            IERC20(Erc20Contract).balanceOf(msg.sender) >= Amount,
-            "sender does not have enought coins"
-        );
-
-        // create the order
-        OrderInfo[msg.sender].Amount = msg.value;
-        OrderInfo[msg.sender].Erc20Contract = Erc20Contract;
-        OrderInfo[msg.sender].ListerAddress = ListerAddress;
-        OrderInfo[msg.sender].NftTokenAddress = ListerNftContract;
-        OrderInfo[msg.sender].TokenId = TokenID;
-
-        //Emit new Order to chain
-        emit newErc20Order(
-            ListerAddress,
+        IERC721(OrderInfo[Buyer][OrderID].BuyerTokenAddress).transferFrom(
+            Buyer,
             msg.sender,
-            ListerNftContract,
-            TokenID,
-            Erc20Contract,
-            msg.value
+            OrderInfo[Buyer][OrderID].BuyerTokenId
         );
-    }
-
-    function takeNftToERC20(
-        address Erc20Token,
-        address NftAddress,
-        address _to,
-        uint256 NftId,
-        uint256 Erc20Amount
-    ) public {
-        // only the token owner can create the order
-        require(
-            IERC721(NftAddress).ownerOf(NftId) == msg.sender,
-            "not owner, cannot create order"
+        //transfer the order taker token from taker to maker
+        IERC721(OrderInfo[Buyer][OrderID].ListerTokenAddress).transferFrom(
+            msg.sender,
+            Buyer,
+            OrderInfo[Buyer][OrderID].ListerTokenId
         );
-
-        // transfer Nft to person who just bought it
-        IERC721(NftAddress).transferFrom(_to, msg.sender, NftId);
-        // transfer KyleCoins (ERC-20) to person who just sold NFT
-        IERC20(Erc20Token).transferFrom(msg.sender, _to, Erc20Amount);
+        // Delete Order
+        delete OrderInfo[Buyer][OrderID];
     }
 }
--- test/testSwap.js
+++ test/testSwap.js
@@ -1,7 +1,6 @@
 const NftSwap = artifacts.require('NftSwap')
 const TokenOneMinter = artifacts.require('TokenOneMinter')
 const TokenTwoMinter = artifacts.require('TokenTwoMinter')
-const KyleCoin = artifacts.require('Erc20Minter')
 
 const { expect } = require('chai')
 
@@ -13,9 +12,6 @@ let owner
 let maker
 let taker
 let lister
-
-let kyleCoin
-let kyleCoinAddress
 
 const makerID = 1
 const takerID = 2
@@ -43,7 +39,7 @@ describe('Test Swap', () => {
 	it("Mint NFT's in both NFT contracts", async () => {
 		await nftOne.mint(maker, makerID)
 		await nftTwo.mint(taker, takerID)
-		await nftOne.mint(lister, 714);
+
 
 		expect(await nftOne.ownerOf(makerID)).to.be.equal(maker)
 		expect(await nftTwo.ownerOf(takerID)).to.be.equal(taker)
@@ -61,17 +57,9 @@ describe('Test Swap', () => {
 			makerID,
 			nftTwo.address,
 			takerID,
+			71444,
 			{ from: maker }
 		)
-
-		expect(
-			await nftSwap.validRecipient(
-				nftOne.address,
-				makerID,
-				nftTwo.address,
-				takerID
-			)
-		).to.be.true
 	})
 
 	it('Taker approves swap contract', async () => {
@@ -84,95 +72,12 @@ describe('Test Swap', () => {
 		expect(await nftOne.ownerOf(makerID)).to.be.equal(maker)
 		expect(await nftTwo.ownerOf(takerID)).to.be.equal(taker)
 
-		await nftSwap.takeOrder(
-			nftOne.address,
-			makerID,
-			nftTwo.address,
-			takerID,
-			{ from: taker }
-		)
+		let res2;
 
-		expect(await nftOne.ownerOf(makerID)).to.be.equal(taker)
-		expect(await nftTwo.ownerOf(takerID)).to.be.equal(maker)
+		res2 = await nftSwap.takeOrder(maker, 71444, { from: taker })
+
+		console.log(res2);
+		// 	expect(await nftOne.ownerOf(makerID)).to.be.equal(taker)
+		// 	expect(await nftTwo.ownerOf(takerID)).to.be.equal(maker)
 	})
-
-	it('Cannot reswap an already taken swap', async () => {
-		expect(await nftOne.ownerOf(makerID)).to.be.equal(taker)
-		expect(await nftTwo.ownerOf(takerID)).to.be.equal(maker)
-
-		await nftSwap.takeOrder(
-			nftOne.address,
-			makerID,
-			nftTwo.address,
-			takerID,
-			{ from: taker }
-		)
-	})
-})
-
-describe('Test Erc-20 to NFT Swap', () => {
-	before(async () => {
-		accounts = await web3.eth.getAccounts()
-
-		buyer = accounts[0]
-		lister = accounts[3]
-	})
-
-	it("Deploys Correctly", async () => {
-		nft = await TokenOneMinter.new();
-		nftSwap = await NftSwap.new();
-		kyleCoin = await KyleCoin.new();
-	})
-
-	it("Mint Lister NFT contract", async () => {
-		await nft.mint(lister, 714);
-
-		expect(await nft.ownerOf(714)).to.be.equal(lister)
-	})
-
-	it("Mints KyleCoins (ERC20) ", async () => {
-		let totalCoins;
-
-		//Since buyer lanuched, it holds the contract the intial total supply of KC (Kyle Coin);
-		totalCoins = await kyleCoin.balanceOf(buyer);
-		console.log("Total Kyle Coins: " + totalCoins);
-		console.log(totalCoins.ToNumber());
-
-		expect(await kyleCoin.balanceOf(buyer)).to.be.equal(714000)
-	})
-
-	it('Creates Erc20 for Nft Order', async () => {
-		let res;
-		res = await nftSwap.makeNftToERC20(
-			lister,
-			nft.address,
-			714,
-			kyleCoin.address,
-			1470,
-			{ from: buyer }
-		);
-		console.log(res);
-	})
-
-	// function takeNftToERC20(
-	//     address Erc20Token,
-	//     address NftAddress,
-	//     address _to,
-	//     uint256 NftId,
-	//     uint256 Erc20Amount
-	// ) public {
-
-	it('Transfers the Swap', async () => {
-		let res;
-		res = await nftSwap.takeNftToERC20(
-			kyleCoin.address,
-			nft.address,
-			buyer,
-			714,
-			1470,
-			{ from: lister }
-		);
-		console.log(res);
-	})
-
 })
